6304e24dbe5f768e2f2d2710787de68d58ee54c6
MiguelEspezua <miguel.polanco@revature.net>
2021-05-26 16:27:20 -0500

    first version

---
+++ HelloWold/src/main/java/dev/polanco/helloworld/HelloWorld.java
@@ -0,0 +1,21 @@
+
+//This is how we write a comment in java
+
+// This line(package murt always be the 
+package dev.polanco.helloworld;
+
+
+
+/*
+ * Multi Line comment
+ * 
+ * Packages are a way of organizing out code into a hierarchy
+ */
+public class HelloWorld {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("Hello World!");
+	}
+
+}
